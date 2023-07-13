451a81bbb432b9240f2c177fde1a4e9feff0f949
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-13 22:50:46 +0100

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n",61);
+	char so[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+
+	write(1, so, 61);
 	return (1);
 }
