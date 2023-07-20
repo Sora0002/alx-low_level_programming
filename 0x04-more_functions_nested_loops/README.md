9099ccb9e46d9db67a7c884ea3c3a5c408bf1fed
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-20 17:23:26 +0100

    a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+#README file
+~~~
+welcome to my dir
+~~~
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*_Putchar function*/
+
+int _putchar(char c);
+
+/* _issupper function */
+
+int _isupper(int c);
+
+#endif
