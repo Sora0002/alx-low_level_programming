da47ee4706dc56ddb74e0ed5ac7d0aea6c061161
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-20 18:21:18 +0100

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a, d, k, i = 0;
+
+	while (i <= 10)
+	{
+		a = 0;
+		while (a <= 14)
+		{
+			k = a;
+			if (a >= 10)
+			{
+				k = a % 10;
+				d = (a - k) / 10;
+				_putchar(d + '0');
+			}
+			_putchar(k + '0');
+			a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -27,4 +27,8 @@ void print_numbers(void);
 
 void print_most_numbers(void);
 
+/* more_numbers function*/
+
+void more_numbers(void);
+
 #endif
