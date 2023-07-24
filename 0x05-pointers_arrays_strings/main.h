1e100400b1a8c3f8c4ec4b6e2f65269ca453f791
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-25 00:05:22 +0100

    task 6

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -31,4 +31,20 @@ void print_rev(char *s);
 
 void rev_string(char *s);
 
+/*puts2 function*/
+
+void puts2(char *str);
+
+/*puts_half function*/
+
+void puts_half(char *str);
+
+/*print_array function*/
+
+void print_array(int *a, int n);
+
+/*_strcpy function*/
+
+char *_strcpy(char *dest, char *src);
+
 #endif
