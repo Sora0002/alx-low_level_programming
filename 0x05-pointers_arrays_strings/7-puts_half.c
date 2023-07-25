6864728ab022d5454f263eb8819a14f026569bde
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-25 02:26:50 +0100

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 void puts_half(char *str)
 {
 	int a = 0, b;
-	
+
 	while (str[a] != '\0')
 	{
 		a++;
@@ -18,7 +18,7 @@ void puts_half(char *str)
 		a--;
 		b = 0;
 		a = a / 2;
-		while (b <= a)
+		while (b < a)
 		{
 			_putchar(str[b]);
 			b++;
