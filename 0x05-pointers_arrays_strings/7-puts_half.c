3aa5fc390af1da5e4638cebecfb5fe4ff727bfb8
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-25 02:02:14 +0100

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - print half of a string
+ * @str: the string
+ *
+ * Return: vois
+ */
+void puts_half(char *str)
+{
+	int a = 0, b;
+	
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	b = (a / 2);
+	while (b < a)
+	{
+		_putchar(str[b]);
+		b++;
+	}
+	_putchar('\n');
+}
