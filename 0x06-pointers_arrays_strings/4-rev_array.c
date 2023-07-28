67323efd18d74c63e6a91640054294ca6af82028
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-28 02:30:47 +0100

    task 3

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverses the content of an array of integers.
+ * @a: table 1
+ * @n: table 2
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, b = (n - 1) / 2, c = n - 1, temp;
+
+	while (i < b)
+	{
+		temp = a[i];
+		a[i] = a[c];
+		a[c] = temp;
+		i++;
+		c--;
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -23,4 +23,8 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
+/* reverse_array function*/
+
+void reverse_array(int *a, int n);
+
 #endif
