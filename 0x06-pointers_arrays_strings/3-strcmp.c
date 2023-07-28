f8b0cc26c5e36c2a5beb7054403ea00900986cb0
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-28 01:33:44 +0100

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcmp -  a function that compares two strings.
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: size
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, b;
+
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		b = s1[i] - s2[i];
+		if (b == 0)
+		{
+			i++;
+			continue;
+		}
+		else if (b < 0)
+			break;
+		else
+			break;
+	}
+	return (b);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,4 +19,8 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
+/*_strcmp function*/
+
+int _strcmp(char *s1, char *s2);
+
 #endif
