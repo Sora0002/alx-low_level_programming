e3b7bc0763189f27888c25bd37121be5e2154f4c
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-07-31 14:26:59 +0100

    first commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*_Putchar function*/
+
+int _putchar(char c);
+
+/*_memset function*/
+
+char *_memset(char *s, char b, unsigned int n);
+#endif
