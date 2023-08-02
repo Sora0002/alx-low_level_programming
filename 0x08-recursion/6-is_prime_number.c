cc89420e1ff2974e3cd9e57e57d58d475533485e
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-02 22:03:44 +0100

    task 6 not f

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,1 +1,22 @@
+#include "main.h"
+int checker(int n, int b)
+{
+	if (b <= n / 2)
+	{
+		if (n % b == 0)
+			return (0);
+		return (checker(n, b + 1));
+	}
+}
+/**
+ * is_prime_number - function to check if the number is prime.
+ * @n: the number
+ *
+ * Return: 1 if prime and 0 if not
+ */
 int is_prime_number(int n)
+{
+	if (checker(n, 0) == 0)
+		return (0);
+	return(1);
+}
