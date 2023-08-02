fc03b3919b44c38b8be806d8c165e39f7e93bd9f
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-02 21:07:05 +0100

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,36 @@
+#include "main.h"
+/**
+ * frec - find sqrt
+ * @n: the number
+ * @s: start
+ * @e: end
+ *
+ * Return: int
+ */
+int frec(int n, int s, int e)
+{
+	if (s <= e)
+	{
+		int m = (s + e) / 2;
+		int sq = m * m;
+
+		if (sq == n)
+			return (m);
+		if (sq < n)
+			return (frec(n, m + 1, e));
+		return (frec(n, s, m - 1));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - return the natural square.
+ * @n: the number
+ *
+ * Return: square
+ */
 int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (frec(n, 0, n));
+}
