7ea2e10ee0a275f8b90a2957a8687b24b07be967
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-04 22:33:36 +0100

    test

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks for alphabetic character.
+ * @c: The character to check
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase. / 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
