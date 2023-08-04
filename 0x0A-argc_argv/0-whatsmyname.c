50cd8f77ce918c98b28af7f868f8c9d0830e94a9
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-05 00:13:55 +0100

    task 0 -0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg values
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
