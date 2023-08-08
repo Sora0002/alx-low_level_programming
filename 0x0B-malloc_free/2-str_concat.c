62f3dfeb682302dc7393ebd4e37e90adb90add2a
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-09 00:20:08 +0100

    task 2 0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, b = 0;
 	char *p;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
 		i++;
