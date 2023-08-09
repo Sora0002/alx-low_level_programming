e4b38600ec1a46c019b7ca94398a34edcf4c6f92
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-09 01:48:50 +0100

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	while (h < height)
 	{
 		a[h] = (int *)malloc(width * sizeof(int));
-		if (a[i] == NULL)
+		if (a[h] == NULL)
 			return (NULL);
 		h++;
 	}
