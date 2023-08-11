b6a2a445ca5ebf345f52d5b4bc569dc97d834311
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-11 03:13:33 +0100

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,31 @@
 #include "main.h"
 /**
+ * _memset -  a function that fills memory with a constant byte.
+ * @s: the buffer
+ * @b: the byte
+ * @n: the end
+ *
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0, l;
+
+	l = (int)n;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	l--;
+	i = 0;
+	while (i <= l)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
+/**
  * _calloc - function like calloc
  * @nmemb: number
  * @size: the size of parts
@@ -7,18 +33,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned char *p;
-	unsigned int i = 0;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	while (i < nmemb)
-	{
-		p[i] = 0;
-		i++;
-	}
+	_memset(p, 0, nmemb); 
 	return (p);
 }
