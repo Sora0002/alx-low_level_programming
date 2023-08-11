ab3ffd3622ff4b7372371e0d8fbae6f6764febac
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-11 03:23:30 +0100

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -27,9 +27,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * sizeof(int));
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	_memset(p, 0, nmemb * sizeof(int));
+	_memset(p, 0, nmemb * size);
 	return (p);
 }
