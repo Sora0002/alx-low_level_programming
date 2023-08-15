a731d644c55dfafef5968f84a9eb96a68254e957
Azze-el arab Bouferdou <azzelarab.bouferdou.co@gmail.com>
2023-08-14 19:07:34 -0700

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - init dog infos
+ * @d: the struct
+ * @name: dog's name
+ * @age: dog's name
+ * @owner: dog's owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
