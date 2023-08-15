abbbd2215639799bbd9837a16dfdaa5e46e383b7
Azze-el arab Bouferdou <azzelarab.bouferdou.co@gmail.com>
2023-08-14 18:57:14 -0700

    0 0x0e

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_h
+#define DOG_h
+/**
+ * struct dog - a dog infos
+ * @name: the name
+ * @owner: the owner
+ * @age: the age
+ *
+ * Description: rien
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
