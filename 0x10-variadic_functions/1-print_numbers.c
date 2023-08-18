fc334cb4af678b17aa1a6b146f7c6709647796df
AZZE-EL ARAB BOUFERDOU <azzelarab.bouferdou.co@gmail.com>
2023-08-18 03:15:42 +0100

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print all numbers
+ * @separator: separate numbers
+ * @n: counter
+ * Rtturn: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list pt;
+
+	if (!n)
+		return;
+	va_start(pt, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(pt, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(pt);
+}
