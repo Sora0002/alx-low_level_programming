2b7b7e869bb93722707d7d12467c2225829a86f2
AZZE-EL ARAB BOUFERDOU <azelarab.bouferdou.widady@gmail.com>
2023-08-18 01:41:23 +0100

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all args
+ * @n: the count
+ *
+ * Return: the rsult
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i = 0, s = 0;
+
+	va_list ptr;
+	va_start(ptr, n);
+	while (i < n)
+	{
+		s += va_arg(ptr, int);
+		i++;
+	}
+	va_end(ptr);
+	return (s);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define MVARIADIC_FUNCTIONS_H
+/* standard libraries */
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+#endif
