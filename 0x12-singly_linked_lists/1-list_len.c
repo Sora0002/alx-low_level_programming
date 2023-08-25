0954da6dd4eafb07b223a260a7a6d14fae7d77cd
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-08-25 01:27:32 +0100

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - return the length of stringly linked
+ * @h: the linked
+ * Return: the length
+*/
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while(h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
