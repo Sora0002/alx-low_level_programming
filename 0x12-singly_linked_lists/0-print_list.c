cf2efed79da6773a0dea57b1b18d8391166d10da
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-08-25 01:16:10 +0100

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: the array with list_t type
+ * Return: the size of the array
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			i++;
+			continue;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		i++;
+	}
+	return (i);
+}
