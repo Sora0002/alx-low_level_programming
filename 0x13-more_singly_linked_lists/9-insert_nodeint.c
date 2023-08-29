68fac162f28c93d8a7337e824c3b92fe2c3cbede
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-08-30 00:49:06 +0100

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,4 +9,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int i = 0;
+	listint_t *t, *l = malloc(sizeof(listint_t));
+
+	if (head == NULL || l == NULL)
+		return (NULL);
+	t = (*head);
+	l->n = n;
+	if (idx)
+	{
+		while (t)
+		{
+			if (i == idx - 1)
+			{
+				l->next = t->next;
+				t->next = l;
+				return (l);
+			}
+			i++;
+			t = t->next;
+		}
+		free(l);
+		return (NULL);
+	}
+	l->next = (*head);
+	(*head) = l;
+	return (l);
 }
