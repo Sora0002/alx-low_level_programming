06047bf6c2702e95aa1d99af08911b201ff9316b
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-08-30 01:08:28 +0100

    task 10 v3

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *t, *l;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head || !*head)
 		return (-1);
 	t = (*head);
 	if (index)
@@ -31,8 +31,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-		t = (*head)->next;
-		(*head) = t;
+		(*head) = (*head)->next;
+		free(t);
 		return (1);
 	}
 	return (-1);
