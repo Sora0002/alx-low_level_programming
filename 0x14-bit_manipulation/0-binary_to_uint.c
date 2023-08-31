b627f2768115c0b90e7e1c67695bf6a53158bf73
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-09-01 00:20:53 +0100

    project 0x14

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * _strlen - return the length of string
+ * @str: the string
+ *
+ * Return: length
+*/
+int _strlen(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != 0; length++)
+		;
+		return (length);
+}
+/**
+ * _pow - function like pow
+ * @ba: base
+ * @e: the power
+ * Return: result
+*/
+int _pow(int ba, int e)
+{
+	if (e == 0)
+		return (1);
+	else if (e > 0)
+		return (ba * _pow(ba, e - 1));
+}
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: the binary number
+ * Return: the converted number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len, sum = 0, i = 0;
+
+	if (!b)
+		return (0);
+	len = _strlen(b) - 1;
+	while (len >= 0)
+	{
+		if (b[len] == '1' || b[len] == '0')
+		{
+			if (b == 0)
+			{
+				len--;
+				continue;
+			}
+			sum += _pow(2, i);
+			i++;
+			len--;
+			continue;
+		}
+		return (NULL);
+	}
+	return (sum);
+}
