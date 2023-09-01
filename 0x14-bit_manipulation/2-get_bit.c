653bcbbe408d43cef673a344567d643c682b7e93
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-09-01 02:16:30 +0100

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,17 @@
 #include "main.h"
+/**
+ * get_bit - function to get specific bit on specific index
+ * @n: the integer
+ * @index: the position
+ * Return: 1 or 0 or -1 if not found
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int a = 1UL << index, s = sizeof(unsigned long int) * 8;
-	
+
 	if (index >= s)
 	{
-		return -1; 
+		return (-1);
 	}
 	if (n & a)
 	{
