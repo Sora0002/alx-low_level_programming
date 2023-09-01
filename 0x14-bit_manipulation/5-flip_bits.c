2e1c3c58131b5a7025ae5303a34c84eeb4b30135
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-09-01 03:42:31 +0100

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - a function that returns the number of bits you would need to flip
+ * @n: fst number
+ * @m: 2nd number
+ * Return: the count
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int a = n ^ m, b = 1UL;
+	unsigned int i = 0;
+
+	while (a)
+	{
+		if (a & b)
+			i++;
+		a >>= 1;
+	}
+	return (i);
+}
