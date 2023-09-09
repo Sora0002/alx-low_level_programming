ef05da0f36a297895c8966092a02a2ec145ca80e
Sora0002 <azzelarab.bouferdou@gmail.com>
2023-09-09 03:26:13 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	f = popen(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (f == -1)
 		return (-1);
