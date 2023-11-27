199b2cbbdd9d462de1d6ceec435db8c45864c253
Azze-el arab ouferdou <azzelarab.bouferdou@gmail.com>
2023-11-27 22:59:00 +0100

    teste

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,19 +3,19 @@
 
 /**
  * error_file - checks if files can be opened.
- * @from: from.
- * @tofile: tofile.
+ * @file_from: file_from.
+ * @file_to: file_to.
  * @argv: arguments vector.
  * Return: no return.
  */
-void error_file(int from, int tofile, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (from == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (tofile == -1)
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -30,42 +30,42 @@ void error_file(int from, int tofile, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int from, tofile, errc;
-	ssize_t nch, nwr;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp from tofile");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	from = open(argv[1], O_RDONLY);
-	tofile = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(from, tofile, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	nch = 1024;
-	while (nch == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		nch = read(from, buf, 1024);
-		if (nch == -1)
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(tofile, buf, nch);
+		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
-	errc = close(from);
-	if (errc == -1)
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	errc = close(tofile);
-	if (errc == -1)
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
